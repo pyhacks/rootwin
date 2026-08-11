dc612aa4cc44fd2867276553b4e2cf8ef16c345f
pyhacks <enginyildirim111@gmail.com>
2026-08-11 16:56:17 +0300

    update

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import subprocess
+import os
+import shutil
+        
+
+def main():
+    cd = os.getcwd()
+    root_dir = os.path.join(cd, "src", "rootwin", "root")
+    os.chdir(root_dir)
+    print("generating the .pch...")
+    process = subprocess.run("python makepch.py allDict.cxx.pch", shell = True, creationflags = subprocess.CREATE_NO_WINDOW)
+    print("successful.")
+    shutil.move("allDict.cxx.pch", "etc")
+    os.chdir(cd)
+    #setuptools.setup()
+
+
+main()
